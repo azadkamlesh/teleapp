70fa298afb8eb17a385b3be562c9d799b469e3a8
azadkamlesh <kamleshyadavazad@gmail.com>
2025-06-25 00:37:58 +0000

    feat: add Vercel Analytics to Next.js home page

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
+import { Analytics } from "@vercel/analytics/next";
+
 export default function Home() {
   return (
     <div style={{ textAlign: 'center', marginTop: '2rem' }}>
       <h1>Welcome to TeleApp (Next.js)</h1>
+      <Analytics />
     </div>
   );
 }
