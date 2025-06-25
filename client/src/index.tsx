6a63cc6c1bf159f0f5e605e3a6ffac6d64e82c0c
azadkamlesh <kamleshyadavazad@gmail.com>
2025-06-25 00:01:46 +0000

    feat: add Vercel SpeedInsights to app

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 
-const App = () => <h1>Welcome to TeleApp!</h1>;
+const App = () => (
+  <>
+    <h1>Welcome to TeleApp!</h1>
+    <SpeedInsights />
+  </>
+);
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 root.render(<App />);
